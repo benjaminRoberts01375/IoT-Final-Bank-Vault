af4765a7eb992e4f04a28806d03b4032529fede6
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-06 15:29:08 -0400

    Enabled connecting to MQTT broker

--- Arduino Bank Vault/src/mqttConnection.cpp
+++ Arduino Bank Vault/src/mqttConnection.cpp
@@ -1,2 +1,20 @@
 #include "mqttConnection.h"
 
+void mqttConnection::reconnectMQTTClient() {
+  while (!MQTTClient.connected())
+  {
+    Serial.print("Attempting MQTT connection... ");
+    if (MQTTClient.connect(CLIENT_NAME.c_str()))
+    {
+      Serial.println("Connected to broker.");
+      MQTTClient.subscribe(TOPIC.c_str()); // TO SUBCRIBE A TOPIC
+    }
+    else
+    {
+      Serial.print("Retying in 5 seconds - failed, rc=");
+      Serial.println(MQTTClient.state());
+
+      delay(5000);
+    }
+  }
+}
--- Arduino Bank Vault/src/mqttConnection.h
+++ Arduino Bank Vault/src/mqttConnection.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <PubSubClient.h>
+#include <credentials.h>
 #include <string>
+#include "wifiConnection.h"
 
-namespace mqttConnection {
-    
+namespace mqttConnection
+{
+    /// @brief Connect to MQTT client if not already connected (ex. startup or random disconnect)
+    void reconnectMQTTClient();
     PubSubClient MQTTClient(wifiConnection::wifiClient);
 }
