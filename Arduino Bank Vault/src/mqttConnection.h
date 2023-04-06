9a686fa52f7e51bbe0e75320a2419cc60598f5b4
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-06 15:40:05 -0400

    Added MQTTClient documentation

--- Arduino Bank Vault/src/mqttConnection.h
+++ Arduino Bank Vault/src/mqttConnection.h
@@ -6,7 +6,10 @@
 
 namespace mqttConnection
 {
+    /// @brief Setup an MQTT instance
+    /// @param WiFiClient Wifi to use to connect to MQTT broker
+    PubSubClient MQTTClient(wifiConnection::wifiClient);
+    
     /// @brief Connect to MQTT client if not already connected (ex. startup or random disconnect)
     void reconnectMQTTClient();
-    PubSubClient MQTTClient(wifiConnection::wifiClient);
 }
