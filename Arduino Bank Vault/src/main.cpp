55d7d9217d415801190fc9a119e884b48ad6fe24
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-07 01:26:11 -0400

    Better comments

--- Arduino Bank Vault/src/main.cpp
+++ Arduino Bank Vault/src/main.cpp
@@ -4,15 +4,15 @@ int loops = 1;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);                  // Wait for Serial to be ready
-  wifiConnection::connectWiFi();
-  mqttConnection::configureMQTTClient();
+  while (!Serial);                        // Wait for Serial to be ready
+  wifiConnection::connectWiFi();          // Connect to wifi
+  mqttConnection::configureMQTTClient();  // Setup MQTT
   
-  pinMode(LED_R_PIN, OUTPUT);       // Red LED pin
-  pinMode(LED_G_PIN, OUTPUT);       // Green LED pin
-  pinMode(LED_B_PIN, OUTPUT);       // Blue LED pin
-  doorServo.attach(SERVO_PIN);      // Servo pin
-  pinMode(DOOR_PIN, INPUT_PULLUP);  // Button pin
+  pinMode(LED_R_PIN, OUTPUT);             // Red LED pin
+  pinMode(LED_G_PIN, OUTPUT);             // Green LED pin
+  pinMode(LED_B_PIN, OUTPUT);             // Blue LED pin
+  doorServo.attach(SERVO_PIN);            // Servo pin
+  pinMode(DOOR_PIN, INPUT_PULLUP);        // Button pin
   
   digitalWrite(LED_B_PIN, HIGH);    // Set LED to blue
   doorServo.write(0);               // Move servo to starting position
