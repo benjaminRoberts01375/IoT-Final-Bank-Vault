e60869e659593138ae85dc34f66d091549e1629f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-23 16:58:36 -0400

    Created user data storage

---
+++ Bank Vault/Bank Vault/Models/UserLoginM.swift
@@ -0,0 +1,13 @@
+// Ben Roberts
+
+import SwiftUI
+
+final class UserLoginM: ObservableObject {
+    // Setup singleton
+    public static var shared: UserLoginM = UserLoginM()
+    private init() {
+        self.vaults = []
+    }
+    
+    @Published var vaults: [VaultM]
+}
