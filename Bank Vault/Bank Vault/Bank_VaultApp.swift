966900ea7db6dd2ce4a5c81503e75a3d2ebda846
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-23 16:58:17 -0400

    Created front-end application project

---
+++ Bank Vault/Bank Vault/Bank_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bank_VaultApp.swift
+//  Bank Vault
+//
+//  Created by Ben Roberts on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Bank_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
