79680dcfda24ff9fce491fce8489a8b725c3813d
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-23 17:08:39 -0400

    Basic blocking for home screen

--- Bank Vault/Bank Vault/ContentView.swift
+++ Bank Vault/Bank Vault/ContentView.swift
@@ -4,6 +4,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationStack {
+            List {
+                ForEach(Array(UserLoginM.shared.vaults.enumerated()), id: \.element.id) { index, vault in
+                    Button(action: {
+                        
+                    }, label: {
+                        Text(vault.name)
+                    })
+                    .transition(.opacity)
+                }
+            }
+            .edgesIgnoringSafeArea([.bottom, .horizontal])
+            .navigationTitle("Your Vaults")
+            .toolbar {
+                Button(action: {
+                    print("Toggle edit mode")
+                }, label: {
+                    Text("Edit")
+                })
+            }
+        }
     }
 }
 
