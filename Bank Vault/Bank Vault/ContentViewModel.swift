ce2d7cbf5273b64f4186303ec201bfe1d9f6b46e
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-24 01:59:32 -0400

    Tracker for displaying a login page

--- Bank Vault/Bank Vault/ContentViewModel.swift
+++ Bank Vault/Bank Vault/ContentViewModel.swift
@@ -3,9 +3,13 @@
 import SwiftUI
 
 final class ContentViewModel: ObservableObject {
+    /// Tracker for showing login page
+    @Published var showLoginPage: Bool
+    /// Tracker for being in edit mode
     @Published var editMode: Bool
     
     init() {
         self.editMode = UserLoginM.shared.vaults.isEmpty
+        self.showLoginPage = true
     }
 }
