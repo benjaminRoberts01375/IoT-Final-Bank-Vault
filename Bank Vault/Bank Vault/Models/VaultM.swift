8259f927e5501ba7fb94d7f6d92789d5f4ab83b3
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-24 23:04:33 -0400

    Correctly changed vault accessor to Int

--- Bank Vault/Bank Vault/Models/VaultM.swift
+++ Bank Vault/Bank Vault/Models/VaultM.swift
@@ -2,8 +2,12 @@
 
 import SwiftUI
 
+/// Basic storage for a vault door's info
 class VaultM: Codable, Identifiable {
+    /// Unique id for the vault
     let id: UUID
+    /// Name of the vault
     let name: String
-    let accessor: String
+    /// 512 hash of user info to determine if the user is allowed to conenct to a vault
+    let accessor: Int
 }
