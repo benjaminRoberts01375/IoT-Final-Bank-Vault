6615ee4974ed04aa8914da9c0fb319c8f43d248f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-26 02:00:47 -0400

    Added a lot of logic to MQTTManagerM

--- Bank Vault/Bank Vault/Models/MQTTManagerM.swift
+++ Bank Vault/Bank Vault/Models/MQTTManagerM.swift
@@ -1,5 +1,44 @@
 // Ben Roberts
 
+import Combine
+import MQTTNIO
+import SwiftUI
+
 final class MQTTManagerM {
+    let broker: String
+    let port: Int
+    var topic: String
+    var setupTopic: String
+    var mqtt: MQTTClient?
+    var cancellablePublisher: AnyCancellable?
+    var cancellableConnection: AnyCancellable?
+    
+    init() {
+        self.broker = "broker.emqx.io"
+        self.port = 1883
+        self.setupTopic = "setup"
+        self.topic = "setup"
+    }
+    
+    private func connect() -> MQTTClient {
+        let mqtt = MQTTClient(                          // MQTT Configuration
+            configuration: .init(
+                target: .host(broker, port: port),          // Host and port
+                protocolVersion: .version5,                 // MQTT v5
+                clientId: "clientId-iOS",                   // Client ID
+                clean: true,                                // I dunno
+                credentials: .none,                         // No credentials
+                willMessage: .none,                         // No leaving message?
+                keepAliveInterval: .seconds(60),            // Timeout
+                reschedulePings: true                       // Do ping when other messages are sent
+            ),
+            eventLoopGroupProvider: .createNew
+        )
+        
+        mqtt.connect()                                                                                          // Connect to configured MQTT broker
+        
+        
+        return mqtt
+    }
     
 }
