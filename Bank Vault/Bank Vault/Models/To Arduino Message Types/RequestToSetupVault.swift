d8556a2d3cee5f343489c92456c0af99582ac67a
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-26 02:14:05 -0400

    Created request to setup vault

--- Bank Vault/Bank Vault/Models/To Arduino Message Types/RequestToSetupVault.swift
+++ Bank Vault/Bank Vault/Models/To Arduino Message Types/RequestToSetupVault.swift
@@ -1,10 +1,10 @@
 // Ben Roberts
 
-struct CheckUnconfiguredVaults: Encodable {
+struct RequestToSetupVault: Encodable {
     /// Vault to request from
     let vault: VaultM
     /// ID of the user from this device
     let phoneID: Int = UserM.shared.userID ?? 0
     /// Type of request for Arduino
-    let requestType: String = "check setup"
+    let requestType: String = "request setup"
 }
