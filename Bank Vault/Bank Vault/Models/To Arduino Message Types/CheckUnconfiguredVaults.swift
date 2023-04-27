aa6d37b35c1cb93c97a0e11132904fc843833d65
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-27 00:25:12 -0400

    Remove the vault from the check request

--- Bank Vault/Bank Vault/Models/To Arduino Message Types/CheckUnconfiguredVaults.swift
+++ Bank Vault/Bank Vault/Models/To Arduino Message Types/CheckUnconfiguredVaults.swift
@@ -1,8 +1,6 @@
 // Ben Roberts
 
 struct CheckUnconfiguredVaults: Encodable {
-    /// Vault to request from
-    let vault: VaultM
     /// ID of the user from this device
     let phoneID: Int = UserM.shared.userID ?? 0
     /// Type of request for Arduino
