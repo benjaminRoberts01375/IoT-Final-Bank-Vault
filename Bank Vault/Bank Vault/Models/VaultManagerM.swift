269866d2bd0d74c8635666fbf22056300424503d
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-30 05:44:18 -0400

    Docs

--- Bank Vault/Bank Vault/Models/VaultManagerM.swift
+++ Bank Vault/Bank Vault/Models/VaultManagerM.swift
@@ -33,14 +33,19 @@ final class VaultManagerM: ObservableObject {
         mqtt.checkVaultsToSetup()
     }
     
+    /// Boiler plate function for connecting to MQTT
     public func connectMQTT() {
         mqtt.connect()
     }
     
+    /// Boilerplate function for completing the setup for a vault
+    /// - Parameter vault: Vault to setup
     public func addVault(vault: VaultM) {
         mqtt.setupVault(vault)
     }
     
+    /// Adds to the history of a vault
+    /// - Parameter interaction: Interaction taken from a vault
     public func addVaultInteraction(_ interaction: ArduinoInteracted) {
         if let vault = userVaults.first(where: { $0.id == interaction.vault.id }) {                     // Check if we have the vault setup on our end
             if !(vault.history?.contains(where: { $0.time == interaction.doorStatus.time }) ?? true) {      // If the timecode doesn't exist in our records
